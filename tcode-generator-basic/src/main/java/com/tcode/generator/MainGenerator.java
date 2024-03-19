f6732bed111563efa9c2d40670fd9b6fee20faf0
Sevenacs <12409121+sevenancestor@user.noreply.gitee.com>
2024-03-19 19:16:13 +0800

    初始化项目

---
+++ tcode-generator-basic/src/main/java/com/tcode/generator/MainGenerator.java
@@ -0,0 +1,42 @@
+package com.tcode.generator;
+
+import com.tcode.model.MainTemplateConfig;
+import freemarker.template.TemplateException;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 核心生成器
+ */
+public class MainGenerator {
+
+    /**
+     * 生成
+     *
+     * @param model 数据模型
+     * @throws TemplateException
+     * @throws IOException
+     */
+    public static void doGenerate(Object model) throws TemplateException, IOException {
+        String projectPath = System.getProperty("user.dir");
+        // 整个项目的根路径
+        File parentFile = new File(projectPath).getParentFile();
+        // 输入路径
+        String inputPath = new File(parentFile, "tcode-generator-demo-projects/acm-template").getAbsolutePath();
+        String outputPath = projectPath;
+        // 生成静态文件
+        StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
+        // 生成动态文件
+        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/tcode/acm/MainTemplate.java";
+        DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+    }
+
+    public static void main(String[] args) throws TemplateException, IOException {
+        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+        mainTemplateConfig.setAuthor("tcode");
+        mainTemplateConfig.setLoop(false);
+        mainTemplateConfig.setOutputText("求和结果：");
+        doGenerate(mainTemplateConfig);
+    }
+}
